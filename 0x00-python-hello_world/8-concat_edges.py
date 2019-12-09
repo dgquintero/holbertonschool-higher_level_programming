a58d1799c81314bfa537cd95f89757e5f378cbe5
dgquintero <dgquintero02@hotmail.com>
2019-12-09 23:21:48 +0000

    8 answer

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+str = str[39:65] + ' ' + str[-22:-18] + ' ' + str[:6]
 print(str)
