3b2a264234ffe207526e5a7f067d89721a294fdf
dgquintero <dgquintero02@hotmail.com>
2019-12-18 04:16:43 +0000

    55 answer

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    new_s = ""
+    for tmp in my_string:
+        if tmp == 'c' or tmp == 'C':
+            continue
+        new_s = new_s + tmp
+    return new_s
