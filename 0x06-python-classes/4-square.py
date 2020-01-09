0be20a020c96f0f0291b9f112d8aa5cf2c3c370c
dgquintero <dgquintero02@hotmail.com>
2020-01-09 21:20:23 +0000

    3 fixed

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,5 +13,5 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
 
-    def area(self):
-        return (self.__size ** 2)
+        def area(self):
+            return (self.__size ** 2)
