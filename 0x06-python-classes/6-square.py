c48af9820396b50683e51433f2c5e81903771fa6
dgquintero <dgquintero02@hotmail.com>
2020-01-09 23:17:54 +0000

    6 task added

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""class call it Square"""
+
+
+class Square:
+    """class call it Square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """size is a private attribute"""
+        self.__size = size
+        self.__position = position
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size is a private attribute"""
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for k in range(self.__position[0]):
+                    print(" ", end='')
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        self.__position = value
+        if len(self.__position) is not 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
