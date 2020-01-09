3ea94ef456aa9381cf80cbd2537dbdc104dc6d56
dgquintero <dgquintero02@hotmail.com>
2020-01-09 19:55:22 +0000

    0-1 answer

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+"""empty class call it Square"""
+class Square:
+    """empty class call it Square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""class call it Square"""
+class Square:
+    """class call it Square"""
+    def __init__(self, size):
+        """size is a private attribute"""
+        self.__size = size
