2d0bb18d49162af4211a93d8ebce5d1e4214ea91
dgquintero <dgquintero02@hotmail.com>
2020-01-10 01:03:22 +0000

    6 fixed

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,8 +7,8 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """size is a private attribute"""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -31,15 +31,17 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def my_print(self):
-        if self.__size == 0:
-            print()
-        else:
+        if self.__size != 0:
+            for y in range(self.__position[1]):
+                print()
             for i in range(self.__size):
                 for k in range(self.__position[0]):
                     print(" ", end='')
                 for j in range(self.__size):
                     print("#", end='')
                 print()
+        else:
+            print()
 
     @property
     def position(self):
@@ -48,9 +50,9 @@ class Square:
     @position.setter
     def position(self, value):
         self.__position = value
-        if len(self.__position) is not 2 or type(self.__position) is not tuple:
+        if len(value) is not 2 or type(value) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if (self.__position[0]) is not int or self.__position[1] is not int:
+        if any(type(h) is not int for h in value):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if self.__position[0] < 0 or self.__position[1] < 1:
+        if self.__position[0] < 0 or self.__position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
