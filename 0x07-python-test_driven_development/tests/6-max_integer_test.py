d99352f515d311452d80bfb8c49b42430ff038eb
dgquintero <dgquintero02@hotmail.com>
2020-01-14 00:25:34 +0000

    test cases

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""UNittest for the function max_integer"""
+
+
+import unittest
+max_integer = __import__("6-max_integer").max_integer
+
+
+class MyTestCase(unittest.TestCase):
+""" Unittest class with some test casses for the function max_integer"""
