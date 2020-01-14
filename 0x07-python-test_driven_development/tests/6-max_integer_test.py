9636a67d046cd1dfd2bee8da82359ce7ca08e39c
dgquintero <dgquintero02@hotmail.com>
2020-01-14 04:59:39 +0000

    tes

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,4 +7,12 @@ max_integer = __import__("6-max_integer").max_integer
 
 
 class MyTestCase(unittest.TestCase):
-""" Unittest class with some test casses for the function max_integer"""
+    """Unittest class with some test casses for the function max_integer"""
+    def testmaxinteger(self):
+        self.assertEqual(max_integer([1, 28, 3]), 28)
+        self.assertEqual(max_integer(), None)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer("abcdefghijklmnopqrstuvwxyz"), 'z')
+        self.assertIsNone(max_integer())
+if __name__ == "__main__":
+    unittest.main()
