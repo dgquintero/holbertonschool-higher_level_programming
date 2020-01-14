414ec607e128d7fb36a761b60b42a25c83697ec7
dgquintero <dgquintero02@hotmail.com>
2020-01-14 20:30:14 +0000

    1 fixed

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
         """width and height are private attribute"""
         self.__width = width
         self.__height = height
-        """if type(width) is not int:
+        if type(width) is not int:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("size must be >= 0")
@@ -17,7 +17,7 @@ class Rectangle:
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height < 0:
-            raise ValueError("height must be >= 0")"""
+            raise ValueError("height must be >= 0")
     @property
     def width(self):
         return self.__width
@@ -29,14 +29,6 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-
-    @height.setter
-    def height(self, value):
-        self.__height = value
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
 
     @property
     def height(self):
