fbf725867192ad312eb9835ac43d3adaf1d4abb8
dgquintero <dgquintero02@hotmail.com>
2020-01-21 20:54:09 +0000

    0x08 project

--- 0x04-python-more_data_structures/12-prueba.py
+++ 0x04-python-more_data_structures/12-prueba.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     conrom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     value = 0
-    if roman_string is not None or roman_string is str:
+    if (roman_string is not None) and (is isinstance(roman_string, str)):
         for i in roman_string:
             if len(roman_string) > 1:
                 for i in roman_string:
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    conrom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    c = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     value = 0
-    if roman_string is not None or roman_string is str:
-        for i in roman_string:
-            if i in conrom.keys():
-                value += conrom[i]
+    if (roman_string is not None) and (isinstance(roman_string, str)):
+        if len(roman_string) > 1:
+            for i in range(1, len(roman_string)):
+                if c[roman_string[i]] < c[roman_string[i - 1]]:
+                    value += c[roman_string[i]]
+                else:
+                    value += c[roman_string[i]] - 2 * c[roman_string[i - 1]]
+        else:
+            value = c[roman_string[0]]
         return value
     else:
         return 0
