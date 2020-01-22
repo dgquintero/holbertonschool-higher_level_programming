efa83186db0ffa6e6bdba265337e1fa11f9b1392
dgquintero <dgquintero02@hotmail.com>
2020-01-22 00:31:05 +0000

    8 amswer added

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -4,5 +4,5 @@ import json
 
 def load_from_json_file(filename):
     """function that reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, mode='w', encoding='utf-8') as f: 
+    with open(filename, mode='w', encoding='utf-8') as f:
         json.load(my_obj, f)
