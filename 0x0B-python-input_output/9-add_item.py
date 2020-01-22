23869c3dd0340227161c53aaecdd72e219a32e5b
dgquintero <dgquintero02@hotmail.com>
2020-01-22 02:23:22 +0000

    9 answer

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,8 +2,8 @@
 from sys import argv
 
 
-save_to_json = __import__('7-save_to_json_file.py').save_to_json_file
-load_from_json = __import__('8-load_from_json_file.py').load_from_json_file
+save_to_json = __import__('7-save_to_json_file').save_to_json_file
+load_from_json = __import__('8-load_from_json_file').load_from_json_file
 
 filename = 'add_item.json'
 
