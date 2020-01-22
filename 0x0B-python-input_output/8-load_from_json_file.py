405b607c78b00c6b87ad215df08c268dd3f2d6f6
dgquintero <dgquintero02@hotmail.com>
2020-01-22 00:34:50 +0000

    8 amswer fixed

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -5,4 +5,4 @@ import json
 def load_from_json_file(filename):
     """function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, mode='r', encoding='utf-8') as f:
-        json.load(f)
+        return json.load(f)
