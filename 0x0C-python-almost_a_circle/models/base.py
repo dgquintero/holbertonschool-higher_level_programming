cd100973905a2ad8df8244e00ea6f929626761e4
dgquintero <dgquintero02@hotmail.com>
2020-01-27 18:15:00 +0000

    16 str to json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 
 
 class Base:
@@ -12,3 +13,8 @@ class Base:
 
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
