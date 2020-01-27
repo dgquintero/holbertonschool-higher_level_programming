3794cb2e1c45d6c8f8e2b2b014f094f23d1becd0
dgquintero <dgquintero02@hotmail.com>
2020-01-27 18:34:36 +0000

    comments fixed for base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import json
+"""Module for Base class."""
 
 
 class Base:
+    """Class Base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize a Base"""
         if id is not None:
             self.id = id
 
@@ -15,6 +18,7 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """returns the representation of a str to json"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
