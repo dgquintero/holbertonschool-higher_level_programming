6743195df68aa993391c46c9b070c04e43891ad5
dgquintero <dgquintero02@hotmail.com>
2020-01-27 19:39:10 +0000

    modified

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,8 +8,8 @@ class Square(Rectangle):
     """class Square"""
     def __init__(self, size, x=0, y=0, id=None):
         """initialize square"""
-        self.size = size
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     @property
     def size(self):
@@ -27,7 +27,7 @@ class Square(Rectangle):
         return ("[Square] ({}) {}/{} - {}".format(self.id,
                                                   self.x,
                                                   self.y,
-                                                  self.__size))
+                                                  self.width))
 
     def update(self, *args, **kwargs):
         """Update the class square"""
