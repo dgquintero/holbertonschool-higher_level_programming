00f80ac7192cfa85d49987c13e90834818d365f6
dgquintero <dgquintero02@hotmail.com>
2020-01-27 19:43:13 +0000

    modified args

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -36,7 +36,7 @@ class Square(Rectangle):
                 if idx == 0:
                     self.id = value
                 elif idx == 1:
-                    self.__size = value
+                    self.width = value
                 elif idx == 2:
                     self.x = value
                 elif idx == 3:
