2252bc51a58c5b07c50583d92cbc1726cd317428
dgquintero <dgquintero02@hotmail.com>
2020-01-27 20:00:59 +0000

    modified def def jsonto str

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,7 +17,8 @@ class Base:
 
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
+            
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the representation of a str to json"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
