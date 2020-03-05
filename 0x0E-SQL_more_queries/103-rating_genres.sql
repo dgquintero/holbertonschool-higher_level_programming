2d9060ccd62894aa112ee4d8eebb422d850be200
Daniel Quintero <dgquintero02@hotmail.com>
2020-03-05 16:05:25 -0500

    Update 103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -7,8 +7,8 @@
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres 
-ON tv_genres.id = tv_show_genres.show_id
+ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_show_ratings
 ON tv_show_genres.show_id = tv_show_ratings.show_id
-GROUP BY tv_genres.name
+GROUP BY tv_genres.id
 ORDER BY rating DESC;
