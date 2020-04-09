64842ea6d552d629d2d48ddeab4891cf30a06057
dgquintero <dgquintero02@hotmail.com>
2020-04-09 22:16:06 +0000

    request fixed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the size body of the response
-$ curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
