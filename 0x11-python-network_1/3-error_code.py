dc12bb08ffa61bfbd72867bdab4910dd27bef455
dgquintero <dgquintero02@hotmail.com>
2020-04-17 02:54:22 +0000

    adding error_code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,7 +4,6 @@ script that takes in a URL and an email, sends a POST request to the passed
 URL with the email as a parameter, and displays the body of the response
 """
 
-import urllib.parse as parse
 import urllib.request as request
 import urllib.error as error
 from sys import argv
@@ -12,6 +11,8 @@ from sys import argv
 
 if __name__ == "__main__":
     r = request.Request(argv[1])
-    try: request.urlopen(r)
+    try:
+        with request.urlopen(r) as re:
+            print(re.read().decode('utf-8'))
     except error.URLError as e:
-        print(e.reason)
+        print('Error code:', e.code)
