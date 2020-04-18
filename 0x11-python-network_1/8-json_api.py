d6033dc98cb1e59f98dfa83c452b033f10756216
dgquintero <dgquintero02@hotmail.com>
2020-04-18 00:43:00 +0000

    8 fixed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,13 +6,14 @@ import requests
 from sys import argv
 
 
+def main():
+    if len(argv) == 1 or argv[1].isdigit():
+        print('No result')
+    else:
+        r = requests.post('http://0.0.0.0:5000/search_user',
+                          data={'q': argv[1]})
+        k = r.json()
+        print('[{}] {}'.format(k.get('id'), k.get('name')))
+
 if __name__ == "__main__":
-    def main():
-        if len(argv) == 1 or argv[1].isdigit():
-            print('No result')
-        else:
-            r = requests.post('http://0.0.0.0:5000/search_user',
-                              data={'q': argv[1]})
-            k = r.json()
-            print('[{}] {}'.format(k.get('id'), k.get('name')))
     main()
