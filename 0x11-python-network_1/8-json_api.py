4ae01a4d26c183cdd1fc19e325f1d86142dcc6f3
dgquintero <dgquintero02@hotmail.com>
2020-04-19 23:49:23 +0000

    fixing 8 file and using 10

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,14 +7,12 @@ from sys import argv
 
 if __name__ == "__main__":
     values = {'q': ""}
+    url = 'http://0.0.0.0:5000/search_user'
     if len(argv) == 2:
         values['q'] = argv[1]
-        r = requests.post('http://0.0.0.0:5000/search_user',
-                          data={'q': argv[1]})
+        r = requests.post(url, data=values)
     else:
-        
-            r = requests.post('http://0.0.0.0:5000/search_user',
-                              data={'q': argv[1]})
+            r = requests.post(url, data=values)
     try:
             k = r.json()
             if k:
