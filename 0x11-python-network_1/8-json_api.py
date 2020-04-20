c450f37a0722ab0c376199e1411d8996ed0daf3c
dgquintero <dgquintero02@hotmail.com>
2020-04-20 00:19:18 +0000

    fixing 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,15 +9,16 @@ if __name__ == "__main__":
     values = {'q': ""}
     url = 'http://0.0.0.0:5000/search_user'
     if len(argv) == 2:
-        values['q'] = argv[1]
+        word_passed = argv[1]
+        values['q'] = word_passed
         r = requests.post(url, data=values)
     else:
-            r = requests.post(url, data=values)
+        r = requests.post(url, data=values)
     try:
-            k = r.json()
-            if k:
-                print('[{}] {}'.format(k.get('id'), k.get('name')))
-            else:
-                print('No result')
+        k = r.json()
+        if k:
+            print('[{}] {}'.format(k.get('id'), k.get('name')))
+        else:
+            print('No result')
     except:
         print('Not a valid Json')
