34f4002e4ac1383db9ea8d650c7320d3863ebd86
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-05 17:05:08 -0500

    9 add fixed

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,10 +2,10 @@
 // script that prints the addition of 2 integers
 const a = process.argv[2];
 const b = process.argv[3];
-function add(a, b) {
+function add (a, b) {
   if (isNaN(a) || isNaN(b)) {
     return (NaN);
   }
-  return (parseInt(a) + parseInt(b));
+  return (Number(a) + Number(b));
 }
 console.log(add(a, b));
