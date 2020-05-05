b76a2ab9fe81f2d488087413b626ca7b259c797b
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-05 17:06:28 -0500

    10 add fixed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 /* script that prints “Javascript is amazing” */
 const num = process.argv[2];
-function factorial(num) {
+function factorial (num) {
   if (isNaN(num) || num === 1) {
     return 1;
   }
   return (num * factorial(num - 1));
 }
-console.log(factorial(parseInt(num)));
+console.log(factorial(Number(num)));
