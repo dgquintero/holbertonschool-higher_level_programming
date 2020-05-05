1d02e8d7263709457c1c80a15a151159b0ccfc62
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-05 17:39:34 -0500

    11 fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-
-let myArgs = process.argv.slice(2);
+// script that searches the second biggest integer in the list
 let secondMax = 0;
+const myArgs = process.argv.slice(2);
 if (myArgs.length > 1) {
   myArgs.sort();
   secondMax = myArgs[myArgs.length - 2];
