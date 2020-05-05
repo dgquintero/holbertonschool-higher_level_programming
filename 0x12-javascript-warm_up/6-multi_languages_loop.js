e91a426684ebd1d10661dbd01efe86b272d04ba4
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-05 16:25:09 -0500

    6 fixed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-
 /* script that prints 3 lines */
-
 const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
 for (const element of myVar) {
   console.log(element);
 }
