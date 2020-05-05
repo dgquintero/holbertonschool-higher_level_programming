188625362326f02a76b68cc6d381999a55ff9ee5
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-05 16:14:17 -0500

    5 fixed

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
- // Write a script that prints My number: <first argument converted in integer
-
+// Write a script that prints My number: <first argument converted in integer
 const myArgs = process.argv[2];
 if (isNaN(myArgs)) {
   console.log('Not a number');
