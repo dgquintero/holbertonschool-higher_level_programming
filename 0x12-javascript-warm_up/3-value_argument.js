45c077f9a95d5cf7d1ab01b1fbeb2f8e275416a9
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-05 16:10:59 -0500

    3 fixed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
- // script that prints the first argument passed to it
-
+// script that prints the first argument passed to it
 const myArgs = process.argv[2];
 if (myArgs === undefined) {
   console.log('No argument');
