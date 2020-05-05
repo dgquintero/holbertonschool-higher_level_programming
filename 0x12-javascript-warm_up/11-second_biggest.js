158981946b6335d0422a1eb13da7e7b052d3a474
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-05 17:32:18 -0500

    11 fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+
 const myArgs = process.argv.slice(2);
 let secondMax = 0;
-if (myArgs.length > 3) {
+if (myArgs.length > 1) {
   myArgs.sort();
   secondMax = myArgs[myArgs.length - 2];
 }
