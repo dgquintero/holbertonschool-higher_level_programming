69f5fb2c73f62ae774267d11639e8b03095f5713
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-05 16:37:07 -0500

    9 add fixed

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,8 +5,8 @@ const b = process.argv[3];
 function add(a, b) {
   if (isNaN(a) || isNaN(b)) {
     return (NaN);
+  } else {
+  return (Number(a) + Number(b));
   }
-  const sum = Number(a) + Number(b);
-  return sum;
 }
 console.log(add(a, b));
