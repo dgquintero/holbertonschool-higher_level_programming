986fad9c9d646c7267f1a87b6811a5d836cedb64
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-11 13:45:25 -0500

    Adding an instance method to square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// class Square that defines a square and inherits from Rectangle
+const Square1 = require('./5-square.js');
+
+module.exports = class Square extends Square1 {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
+      }
+    }
+  }
+};
