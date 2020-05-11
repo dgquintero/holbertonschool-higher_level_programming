85784858ae5c694aa3d8b7cc3fe8ee7e338f730f
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-11 12:30:32 -0500

    Semistandard format fixed

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,26 +1,29 @@
 #!/usr/bin/node
 // class Rectangle that defines a rectangle
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
   // instance Method
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
+
   // instance Method
-  rotate() {
-    let tmp = this.height;
+  rotate () {
+    const tmp = this.height;
     this.height = this.width;
     this.width = tmp;
   }
+
   // instance Method
-  double() {
+  double () {
     this.width = this.width * 2;
     this.height = this.height * 2;
   }
