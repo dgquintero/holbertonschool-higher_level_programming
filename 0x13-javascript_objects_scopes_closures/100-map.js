09eb866f2817968564f7b28a5382dc6f85b68f6f
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-11 15:52:37 -0500

    using map function file

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// script that imports an array and computes a new array.
+const list = require('./100-data.js').list;
+console.log(list);
+const map1 = list.map((x, index) => x * index);
+console.log(map1);
