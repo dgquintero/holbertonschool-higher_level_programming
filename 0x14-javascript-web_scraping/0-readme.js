fd69d774b8b8db2b84c7cea5831d601264994791
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-12 19:42:12 -0500

    adding permissions

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-// Write a script that reads and prints the content of a file.
+// Write a script that reads and prints the content of a file
 const myArg = process.argv[2];
 const fs = require('fs');
 
