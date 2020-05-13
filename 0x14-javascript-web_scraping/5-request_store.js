d7565410c8cab1f36e2283312029f0ff3e7f98e2
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-13 00:35:46 -0500

    adding content into a fil loripsum web page

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+// Write a script that gets the contents of
+// a webpage and stores it in a file
+const request = require('request');
+const fs = require('fs');
+const file = process.argv[3];
+const url = process.argv[2];
+const options = {
+  url: url,
+  method: 'GET'
+};
+request(options, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(file, body, err => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
