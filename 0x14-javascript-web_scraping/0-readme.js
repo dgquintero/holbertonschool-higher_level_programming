2e640db20015db8f5adf29823dc9050e8b14caad
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-12 19:39:26 -0500

    Project Web_scraping update

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+// Write a script that reads and prints the content of a file.
+const myArg = process.argv[2];
+const fs = require('fs');
+
+fs.readFile(myArg, 'utf-8', (err, data) => {
+    if(err) {
+        throw err;
+    }
+    console.log(data);
+});
